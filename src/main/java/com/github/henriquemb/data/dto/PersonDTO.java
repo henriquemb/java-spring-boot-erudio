b36cc88b931c16c7bd193b620648306a64b9a96a
Moacir Henrique Morais Baruffi <moacirhenrique01@hotmail.com>
2026-04-14 21:38:17 -0300

    77. Custom JSON Serialization

--- src/main/java/com/github/henriquemb/data/dto/PersonDTO.java
+++ src/main/java/com/github/henriquemb/data/dto/PersonDTO.java
@@ -1,17 +1,29 @@
 package com.github.henriquemb.data.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"})
 public class PersonDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
+
     private String address;
+
+    @JsonIgnore
     private String gender;
 
     public PersonDTO() {

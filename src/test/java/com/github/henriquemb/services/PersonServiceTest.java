7c58454ac53d1d4a6164551c2d9051d3221cf659
Moacir Henrique Morais Baruffi <moacirhenrique01@hotmail.com>
2026-04-18 21:08:46 -0300

    93. Testando os Métodos de Create, Update e Delete com Mockito e JUnit 5

--- src/test/java/com/github/henriquemb/services/PersonServiceTest.java
+++ src/test/java/com/github/henriquemb/services/PersonServiceTest.java
@@ -4,7 +4,6 @@ import com.github.henriquemb.data.dto.PersonDTO;
 import com.github.henriquemb.mapper.ObjectMapper;
 import com.github.henriquemb.model.Person;
 import com.github.henriquemb.repository.PersonRepository;
-import com.github.henriquemb.unitetests.mapper.ObjectMapperTests;
 import com.github.henriquemb.unitetests.mapper.mocks.MockPerson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,11 +13,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,6 +40,69 @@ class PersonServiceTest {
         person.setId(1L);
         Mockito.when(personRepository.findById(1L)).thenReturn(Optional.of(person));
         PersonDTO result = personService.findById(1L);
+
+        PersonDTO mockPersonDTO = mockPerson.mockDTO(1);
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(mockPersonDTO.getFirstName(), result.getFirstName());
+        assertNotNull(mockPersonDTO.getLastName(), result.getLastName());
+        assertNotNull(mockPersonDTO.getAddress(), result.getAddress());
+        assertNotNull(mockPersonDTO.getGender(), result.getGender());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("self")
+                                && link.getHref().endsWith("/person/1")
+                                && link.getType() != null
+                                && link.getType().equals("GET")
+                )
+        );
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("findAll")
+                                && link.getHref().endsWith("/person")
+                                && link.getType() != null
+                                && link.getType().equals("GET")
+                )
+        );
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("create")
+                                && link.getHref().endsWith("/person")
+                                && link.getType() != null
+                                && link.getType().equals("POST")
+                )
+        );
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("update")
+                                && link.getHref().endsWith("/person/1")
+                                && link.getType() != null
+                                && link.getType().equals("PUT")
+                )
+        );
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("delete")
+                                && link.getHref().endsWith("/person/1")
+                                && link.getType() != null
+                                && link.getType().equals("DELETE")
+                )
+        );
+    }
+
+    @Test
+    void create() {
+        Person person = mockPerson.mockEntity(1);
+        Mockito.when(personRepository.save(person)).thenReturn(person);
+
+        PersonDTO personDTO = mockPerson.mockDTO(1);
+        PersonDTO result = personService.create(personDTO);
 
         PersonDTO mockPersonDTO = ObjectMapper.parseObject(mockPerson.mockEntity(1),  PersonDTO.class);
         assertNotNull(result);
@@ -99,15 +160,78 @@ class PersonServiceTest {
     }
 
     @Test
-    void create() {
-    }
-
-    @Test
     void update() {
+        Person person = mockPerson.mockEntity(1);
+        Mockito.when(personRepository.findById(1L)).thenReturn(Optional.of(person));
+        Mockito.when(personRepository.save(person)).thenReturn(person);
+
+        PersonDTO personDTO = mockPerson.mockDTO(1);
+        PersonDTO result = personService.update(1L, personDTO);
+
+        PersonDTO mockPersonDTO = ObjectMapper.parseObject(mockPerson.mockEntity(1),  PersonDTO.class);
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(mockPersonDTO.getFirstName(), result.getFirstName());
+        assertNotNull(mockPersonDTO.getLastName(), result.getLastName());
+        assertNotNull(mockPersonDTO.getAddress(), result.getAddress());
+        assertNotNull(mockPersonDTO.getGender(), result.getGender());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("self")
+                                && link.getHref().endsWith("/person/1")
+                                && link.getType() != null
+                                && link.getType().equals("GET")
+                )
+        );
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("findAll")
+                                && link.getHref().endsWith("/person")
+                                && link.getType() != null
+                                && link.getType().equals("GET")
+                )
+        );
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("create")
+                                && link.getHref().endsWith("/person")
+                                && link.getType() != null
+                                && link.getType().equals("POST")
+                )
+        );
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("update")
+                                && link.getHref().endsWith("/person/1")
+                                && link.getType() != null
+                                && link.getType().equals("PUT")
+                )
+        );
+
+        assertTrue(result.getLinks().stream()
+                .anyMatch(
+                        link -> link.getRel().value().equals("delete")
+                                && link.getHref().endsWith("/person/1")
+                                && link.getType() != null
+                                && link.getType().equals("DELETE")
+                )
+        );
     }
 
     @Test
     void delete() {
+        Mockito.when(personRepository.existsById(1L)).thenReturn(true);
+
+        personService.delete(1L);
+
+        verify(personRepository, times(1)).existsById(1L);
+        verify(personRepository, times(1)).deleteById(1L);
+        verifyNoMoreInteractions(personRepository);
     }
 
     @Test

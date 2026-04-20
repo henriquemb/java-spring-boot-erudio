77ed44aab1f47249d97e99dc00e030ec43476e45
Moacir Henrique Morais Baruffi <moacirhenrique01@hotmail.com>
2026-04-20 13:03:54 -0300

    98. Definindo a Configuração Básica do Swagger (Open API)

---
+++ src/main/java/com/github/henriquemb/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.github.henriquemb.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    OpenAPI customOpenAPI() {
+        License license = new License()
+                .name("Apache 2.0")
+                .url("https://github.com/henriquemb");
+
+        Info info = new Info()
+                .title("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
+                .version("v1")
+                .description("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
+                .termsOfService("https://github.com/henriquemb")
+                .license(license);
+
+
+        return new OpenAPI()
+                .info(info);
+    }
+}
--- src/main/java/com/github/henriquemb/controller/PersonController.java
+++ src/main/java/com/github/henriquemb/controller/PersonController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/v1/person")
 public class PersonController {
     private final PersonService service;
 

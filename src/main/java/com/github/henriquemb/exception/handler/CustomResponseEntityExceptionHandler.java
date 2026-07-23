9f6ea2b2badcf983986c398dd4954b452d39e105
Moacir Henrique Morais Baruffi <moacirhenrique01@hotmail.com>
2026-07-23 14:30:10 -0300

    164/165. 2102 Definindo as Configurações de Upload e Download

--- src/main/java/com/github/henriquemb/exception/handler/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/github/henriquemb/exception/handler/CustomResponseEntityExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.github.henriquemb.exception.handler;
 
-import com.github.henriquemb.exception.ExceptionResponse;
-import com.github.henriquemb.exception.RequiredObjectIsNullException;
-import com.github.henriquemb.exception.ResourceNotFoundException;
+import com.github.henriquemb.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -48,5 +46,27 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         );
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(FileStorageException.class)
+    public final ResponseEntity<ExceptionResponse> handleFileStorageException(Exception exception, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                exception.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleFileNotFoundException(Exception exception, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                exception.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
